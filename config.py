b8367d5435eb8c74d006f0200dd7748e70ef85c8
aamirxs <121924610+aamirxs@users.noreply.github.com>
2024-09-23 13:26:10 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 # fill in your details
-api_id = 8131280
-api_hash = 'b8bae4ec39d070dad7d0a1245111d154'
-phone = '+917000270588'
+api_id = 25213780
+api_hash = '5a282fd4d8a755b4c8407ffc6e7d06ca'
+phone = '+918815564468'
